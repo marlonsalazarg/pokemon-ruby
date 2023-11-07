021acac88549f25b89d159c83475f233bb652f53
marlonsalazarg <marlon456_456@hotmail.com>
2023-11-07 17:20:56 -0500

    Add menu methods and update show_stats and goodbye methods

--- game.rb
+++ game.rb
@@ -110,5 +110,72 @@ class Game
     end
   end
 
-  
+  # Inicio de metodos del menu
+  def train(jugador, robot)
+    battle_bot = Battle.new(jugador, robot)
+    battle_bot.start
+  end
+
+  # def challenge_leader
+  #   # Complete this
+  # end
+
+  def show_stats
+    puts "Great #{@name_pokemon}"
+    puts "Kind: #{@pokemon_chosen}"
+    puts "Level: #{@player.pokemon_player.level}"
+    puts "Type: #{@player.pokemon_player.type[0]}"
+    puts "Stats:"
+    @player.pokemon_player.stats.each do |key, value|
+      puts "#{key.capitalize}: #{value}"
+    end
+    puts "Experience Points: #{@player.pokemon_player.exp_current}"
+  end
+
+  def goodbye
+    puts "Thanks for playing Pokemon Ruby"
+    puts "This game was created with love by: [Marlon Salazar, Ricardo Mendoza, Lau Lugo, Yull Timoteo]"
+  end
+
+  # def menu
+  #   # Complete this
+  # end
+
+  def print_train(jugador, robot)
+    puts "#{@name} challenge #{robot.name} for training"
+    puts "#{robot.name} has a #{robot.aleatorio_bot_pokemon} level #{robot.level_pokemon}"
+    puts "What do you want to do now?"
+    puts ""
+    puts "1. Fight        2. Leave"
+    print "> "
+    action = nil
+    until action == "Leave"
+      action = gets.chomp
+      case action
+      when "Fight"
+        train(jugador, robot)
+        break
+      end
+    end
+    print_menu
+  end
+
+  def print_challenge_leader(jugador, leader)
+    puts "#{@name} challenge the Gym Leader Brock for a fight!"
+    puts "Brock has a Onix level 10"
+    puts "What do you want to do now?"
+    puts ""
+    puts "1. Fight        2. Leave"
+    print "> "
+    action = nil
+    until action == "Leave"
+      action = gets.chomp
+      case action
+      when "Fight"
+        train(jugador, leader)
+        break
+      end
+    end
+    print_menu
+  end
 end
